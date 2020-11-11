5ca62bd0ea93d6b92804bab0c0dfb1a3d963c45e
Ihor SS <62531809+Sirkevich@users.noreply.github.com>
2020-11-11 15:51:48 +0200

    add console methods

---
+++ app.js
@@ -0,0 +1,8 @@
+const box = document.querySelector("#box");
+
+console.clear("I'm a clear method");
+console.log("I'm a log method");
+console.table("I'm a table method");
+console.count("I'm a count method");
+console.warn("I'm a warn method");
+console.error("I'm an error method");
